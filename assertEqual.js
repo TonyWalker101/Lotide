2bba44ff645fd26ab7a9cc818d1d8f018f68d4d0
Tony <awalker780@gmail.com>
2021-10-21 18:47:26 -0400

    Created Assertion Function

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`Assertion Passed: ✅ ${actual} === ${expected}`);
+  }
+  return console.log(`Assertion Failed: 🛑 ${actual} === ${expected}`);
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
