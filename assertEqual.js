4e313ca9e7dfb08795c5bf0fc832e0ded749dc28
Tony <awalker780@gmail.com>
2021-10-21 19:15:11 -0400

    Fixed === to !==

--- assertEqual.js
+++ assertEqual.js
@@ -2,7 +2,7 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     return console.log(`Assertion Passed: ✅ ${actual} === ${expected}`);
   }
-  return console.log(`Assertion Failed: 🛑 ${actual} === ${expected}`);
+  return console.log(`Assertion Failed: 🛑 ${actual} !== ${expected}`);
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
