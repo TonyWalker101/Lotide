5e293392fe0862d6d9e0a265841abf81f1b86771
Tony <awalker780@gmail.com>
2021-10-22 18:31:39 -0400

    Created eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`Assertion Passed: ✅ ${actual} === ${expected}`);
+  }
+  return console.log(`Assertion Failed: 🛑 ${actual} !== ${expected}`);
+};
+
+/* assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+*/
+
+let eqArrays = (actual, expected) => {
+  if (!actual || actual.length === 0) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i ++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
+
+
