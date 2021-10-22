609c760de0845203cb821a4aa49c555cab6658f1
Tony <awalker780@gmail.com>
2021-10-22 18:51:32 -0400

    Post lint assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -18,6 +18,6 @@ let assertArraysEqual = function(actual, expected) {
 };
 
 assertArraysEqual([0],[0]); // prints Asserion Passed
-assertArraysEqual([1,2,3,"4"],[1,2,3,4]) // prints Assertion Failed
+assertArraysEqual([1,2,3,"4"],[1,2,3,4]); // prints Assertion Failed
 assertArraysEqual(["apples", "bananas"], ["apples", "bananas"]); // prints Assertion Passed
 assertArraysEqual([], ["apples"]); // prints Assertion Failed
