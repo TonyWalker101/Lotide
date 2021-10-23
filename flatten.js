5d3668aed975e2ef8c310d23c4dbe94d490a2000
Tony <awalker780@gmail.com>
2021-10-23 12:52:54 -0400

    Added comments

--- flatten.js
+++ flatten.js
@@ -22,10 +22,10 @@ let flatten = list => {
 
   for (let i = 0; i < list.length; i++) {
     
-    if (!Array.isArray(list[i])) {
+    if (!Array.isArray(list[i])) { // action if list[i] isn't an Array
       newArray.push(list[i]);
     }
-    for (let x = 0; x < list[i].length; x++) {
+    for (let x = 0; x < list[i].length; x++) { // action if list[i] is an Array
       newArray.push(list[i][x]);
     }
   }
