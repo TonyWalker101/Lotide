4cfbfd7f8f3da5b785d3f6d988b8962e35112a29
Tony <awalker780@gmail.com>
2021-10-26 01:11:46 -0400

    Cleaned up logic

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -5,25 +5,16 @@ const assertEqual = function(actual, expected) {
   return console.log(`Assertion Failed: 🛑 ${actual} !== ${expected}`);
 };
 
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1,1);
-
-const includes = (arr, item) => { // checks if one 1 item is found any element of an array
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === item) {
-      return true;
-    }
-  }
-  return false;
-};
+/* assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+*/
 
 const findKeyByValue = (object,value) => {
 
   const answer = [];
 
   for (const obj in object) {
-    if (object[obj].includes(value)) {
+    if (object[obj] === value) {
       return console.log(obj);
     } 
   }
