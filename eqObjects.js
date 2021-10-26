2e2890a57f121999c6d346066990ceb96d7b8cf8
Tony <awalker780@gmail.com>
2021-10-26 19:03:40 -0400

    eqObjects now accounts for arrays

--- eqObjects.js
+++ eqObjects.js
@@ -24,12 +24,28 @@ const eqObjects = function(object1,object2) {
   }
 
   for (const obj in object1) {
-    if (object1[obj] === object2[obj]) {
-      return true;
+    
+    if (Array.isArray(object1[obj])) {
+
+      if (!eqArrays(object1[obj],object2[obj])) {
+
+        return false;
+      } continue;
+    }
+
+    if (object1[obj] !== object2[obj]) {
+      return false;
     }
   }
-  return false;
+  return true;
 };
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
