225d6ffd6782e6857eaac3dac5501262f69e8430
Tony <awalker780@gmail.com>
2021-10-25 21:16:10 -0400

    Created Count Letters function

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`Assertion Passed: ✅ ${actual} === ${expected}`);
+  }
+  return console.log(`Assertion Failed: 🛑 ${actual} !== ${expected}`);
+};
+
+const counterLetters = input => {
+  
+  const answer = {};
+
+  for (const letter of input) {
+
+    if (letter === " ") {
+      continue;
+    }
+    if (answer[letter]) {
+      answer[letter] ++;
+    } else {
+      answer[letter] = 1;
+    }
+  }
+  return answer;
+};
+
+console.log(counterLetters("Hello World"));
+console.log(counterLetters("lighthouse in the house"));
+
+assertEqual(counterLetters("H")["H"], 1);
+
+
