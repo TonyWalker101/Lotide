e773be7cbc0facddf3d5018983bad46f72b950e7
Tony <awalker780@gmail.com>
2021-10-30 18:10:22 -0400

    Exported function to module

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-let eqArrays = (actual, expected) => {
-  if (!actual || actual.length === 0 || actual.length !== expected.length) {
-    return false;
-  }
-  for (let i = 0; i < actual.length; i ++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 let assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -17,8 +7,11 @@ let assertArraysEqual = function(actual, expected) {
   return console.log(`Assertion Failed: 🛑 ${actual} !== ${expected}`);
 };
 
-assertArraysEqual([0],[0]); // prints Asserion Passed
-assertArraysEqual([1,2,3,"4"],[1,2,3,4]); // prints Assertion Failed
-assertArraysEqual(["apples", "bananas"], ["apples", "bananas"]); // prints Assertion Passed
-assertArraysEqual([], ["apples"]); // prints Assertion Failed
+// assertArraysEqual([0],[0]); // prints Asserion Passed
+// assertArraysEqual([1,2,3,"4"],[1,2,3,4]); // prints Assertion Failed
+// assertArraysEqual(["apples", "bananas"], ["apples", "bananas"]); // prints Assertion Passed
+// assertArraysEqual([], ["apples"]); // prints Assertion Failed
+
+module.exports = assertArraysEqual;
+
 
