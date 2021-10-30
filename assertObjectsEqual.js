ab1a28e5460c2a30fd27ae3a8bb92212669d71c4
Tony <awalker780@gmail.com>
2021-10-30 18:18:35 -0400

    Imported functions from module

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,37 +1,6 @@
-let eqArrays = (actual, expected) => {
-  if (!actual || actual.length === 0 || actual.length !== expected.length) {
-    return false;
-  }
-  for (let i = 0; i < actual.length; i ++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
-const eqObjects = function(object1,object2) {
-
-  if (Object.keys(object1).length !== Object.keys(object2).length) { // checks if # of keys match
-    return false;
-  }
-
-  for (const obj in object1) {
-    
-    if (Array.isArray(object1[obj])) { // checks if key is an array
-
-      if (!eqArrays(object1[obj],object2[obj])) { // checks if arrays in both keys match
-
-        return false;
-      } continue;
-    }
-
-    if (object1[obj] !== object2[obj]) { // checks if both keys match
-      return false;
-    }
-  }
-  return true;
-};
+const eqObjects = require("./eqObjects");
 
 const assertObjectsEqual = function(actual, expected) {
 
