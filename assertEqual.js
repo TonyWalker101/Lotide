9d638fa30da93e2d8153115a71d133ac543264b7
Tony <awalker780@gmail.com>
2021-10-30 18:07:45 -0400

    Commented out test cases

--- assertEqual.js
+++ assertEqual.js
@@ -5,7 +5,8 @@ const assertEqual = function(actual, expected) {
   return console.log(`Assertion Failed: 🛑 ${actual} !== ${expected}`);
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
 
 module.exports = assertEqual;
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1,1);
