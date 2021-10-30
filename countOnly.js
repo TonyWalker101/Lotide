5178f3e406af412a6904f252d4c58556d01036eb
Tony <awalker780@gmail.com>
2021-10-30 18:21:20 -0400

    Imported function from module

--- countOnly.js
+++ countOnly.js
@@ -1,9 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`Assertion Passed: ✅ ${actual} === ${expected}`);
-  }
-  return console.log(`Assertion Failed: 🛑 ${actual} !== ${expected}`);
-};
+const assertEqual = require("./assertEqual");
 
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
