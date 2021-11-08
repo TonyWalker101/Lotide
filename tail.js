0308bdd3776068333d2f9b58bb0bd215d1ee157e
Tony <awalker780@gmail.com>
2021-11-08 18:52:49 -0500

    Removed test cases

--- tail.js
+++ tail.js
@@ -1,6 +1,6 @@
 const assertEqual = require("./assertEqual");
 
-let tail = arr => {
+const tail = arr => {
   let answer = [];
   for (let i = 1; i < arr.length; i ++) {
     answer.push(arr[i]);
@@ -8,11 +8,4 @@ let tail = arr => {
   return answer;
 };
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case 2: ...
-// ...
+module.exports = tail;
