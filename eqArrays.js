5f79a19afe0b5d03353ddfddf25b8e04fc1fc141
Tony <awalker780@gmail.com>
2021-11-08 18:56:23 -0500

    Removed test cases

--- eqArrays.js
+++ eqArrays.js
@@ -12,12 +12,5 @@ let eqArrays = (actual, expected) => {
   return true;
 };
 
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
-
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
-
 module.exports = eqArrays;
 
