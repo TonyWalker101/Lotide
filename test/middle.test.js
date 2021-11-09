73a0448b5ddde2455c5f8eb61831291a6e020101
Tony <awalker780@gmail.com>
2021-11-08 22:33:12 -0500

    Converted test cases to M&C

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,5 +1,45 @@
-const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
+const assert = require("chai").assert;
 
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
-assertArraysEqual(middle([1,2,"a",4,5]), ["a"]);
+describe ("#middle", () => {
+  it ("returns [2] for [1,2,3]", () => {
+
+    const actual = middle([1,2,3]);
+    const expected = [2];
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it ("returns [2,3] for [1,2,3,4]", () => {
+
+    const actual = middle([1,2,3,4]);
+    const expected = [2,3];
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it ("returns [] for [1,2]", () => {
+
+    const actual = middle([1,2]);
+    const expected = [];
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it ("returns [] for [1]", () => {
+
+    const actual = middle([1]);
+    const expected = [];
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it ("returns ['a'] for [1,2,'a',4,5]", () => {
+
+    const actual = middle([1,2,'a',4,5]);
+    const expected = ['a'];
+
+    assert.deepEqual(actual, expected);
+  });
+
+});
