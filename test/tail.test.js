51275c7cd88423d8d4c49e1a1fb426d70eda8fcb
Tony <awalker780@gmail.com>
2021-11-08 22:25:17 -0500

    Converted test cases to M&C

--- test/tail.test.js
+++ test/tail.test.js
@@ -1,7 +1,45 @@
-const assertEqual = require("../assertEqual");
 const tail = require("../tail");
+const assert = require("chai").assert;
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe ("#tail", () => {
+  it ("returns [2,3] for [1,2,3]", () => {
+
+    const actual = tail([1,2,3]);
+    const expected = [2,3];
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it ("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+
+    const actual = tail(['Hello', 'Lighthouse', 'Labs']);
+    const expected = ['Lighthouse', 'Labs'];
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it ("returns 2 for the length of ['Hello', 'Lighthouse', 'Labs']", () => {
+
+    const actual = tail(['Hello', 'Lighthouse', 'Labs']).length;
+    const expected = 2;
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it ("returns 'Lighthouse' as the first element of ['Hello', 'Lighthouse', 'Labs']", () => {
+
+    const actual = tail(['Hello', 'Lighthouse', 'Labs'])[0];
+    const expected = 'Lighthouse';
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it ("returns 'Labs' as the second element of ['Hello', 'Lighthouse', 'Labs']", () => {
+
+    const actual = tail(['Hello', 'Lighthouse', 'Labs'])[1];
+    const expected = 'Labs';
+
+    assert.strictEqual(actual, expected);
+  });
+
+});
