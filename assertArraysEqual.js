ec9d4c8b2bba75ecf825d38f392f8f5e58fef210
Tony <awalker780@gmail.com>
2021-11-08 19:07:35 -0500

    Removed test code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -7,11 +7,4 @@ let assertArraysEqual = function(actual, expected) {
   return console.log(`Assertion Failed: 🛑 ${actual} !== ${expected}`);
 };
 
-// assertArraysEqual([0],[0]); // prints Asserion Passed
-// assertArraysEqual([1,2,3,"4"],[1,2,3,4]); // prints Assertion Failed
-// assertArraysEqual(["apples", "bananas"], ["apples", "bananas"]); // prints Assertion Passed
-// assertArraysEqual([], ["apples"]); // prints Assertion Failed
-
 module.exports = assertArraysEqual;
-
-
