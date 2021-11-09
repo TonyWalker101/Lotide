fd45b7710506bba71a6f8d1cd246e09780401404
Tony <awalker780@gmail.com>
2021-11-08 19:07:55 -0500

    Added test code

---
+++ test/assertArraysEqual.test.js
@@ -0,0 +1,7 @@
+const eqArrays = require("../eqArrays");
+const assertArraysEqual = require("../assertArraysEqual");
+
+assertArraysEqual([0],[0]); // prints Asserion Passed
+assertArraysEqual([1,2,3,"4"],[1,2,3,4]); // prints Assertion Failed
+assertArraysEqual(["apples", "bananas"], ["apples", "bananas"]); // prints Assertion Passed
+assertArraysEqual([], ["apples"]); // prints Assertion Failed
