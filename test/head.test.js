18763a5ef52c85df5689a2c9dcecd10788bc68d5
Tony <awalker780@gmail.com>
2021-11-08 20:54:02 -0500

    Fixed test case

--- test/head.test.js
+++ test/head.test.js
@@ -1,7 +1,7 @@
 const assertEqual = require("../assertEqual");
 const head = require("../head");
 
-assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(1,1);
 
 assertEqual(head([5,6,7]), 5);
