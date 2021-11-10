758b5cb101c30866a9e4e0142ab28f4ac01e804e
Tony <awalker780@gmail.com>
2021-11-09 19:47:23 -0500

    Removed test cases

--- findKey.js
+++ findKey.js
@@ -1,5 +1,3 @@
-const assertEqual = require("./assertEqual");
-
 const findKey = function(object, callback) {
 
   for (let obj in object) {
@@ -10,32 +8,5 @@ const findKey = function(object, callback) {
   return undefined;
 };
 
-// test cases
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) , "noma");
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 3) , "Akaleri");
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 10) , undefined);
+module.exports = findKey;
 
