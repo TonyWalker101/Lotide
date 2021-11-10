868fb2e53842bd9ee95d437812a633205ac10956
Tony <awalker780@gmail.com>
2021-11-09 22:18:10 -0500

    Removed test cases

--- map.js
+++ map.js
@@ -1,9 +1,3 @@
-const eqArrays = require("./eqArrays");
-
-const assertArraysEqual = require("./assertArraysEqual");
-
-const words = ["ground", "control", "to", "major", "tom"];
-
 const map = function(array, callback) {
   const results = [];
 
@@ -14,20 +8,7 @@ const map = function(array, callback) {
   return results;
 };
 
-// test cases
-
-const results1 = map(words, word => word[0]);
-
-assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
-
-const results2 = map([1,10,100], x => x * 3);
-
-assertArraysEqual(results2 , [3,30,300]);
-
-const results3 = map(["dog","cat","dog","bird"], x => x + "s");
-
-assertArraysEqual(results3 , ["dogs","cats","dogs","birds"]);
-
+module.exports = map;
 
 
 
