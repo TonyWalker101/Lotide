d2c2bc269a30355707f873bafd25921e47682be3
Tony <awalker780@gmail.com>
2021-11-11 18:12:06 -0500

    Added comments

--- test/countLetters.test.js
+++ test/countLetters.test.js
@@ -4,4 +4,5 @@ const counterLetters = require("../countLetters");
 console.log(counterLetters("Hello World"));
 console.log(counterLetters("lighthouse in the house"));
 
+// Should equal 1
 assertEqual(counterLetters("H")["H"], 1);
