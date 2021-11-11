7cb5ea548ef0c2674579e1b9fe59eee545f87d51
Tony <awalker780@gmail.com>
2021-11-11 18:25:27 -0500

    Removed extra function and added comments

--- test/eqObjects.test.js
+++ test/eqObjects.test.js
@@ -1,18 +1,21 @@
-const eqArrays = require("../eqArrays");
 const assertEqual = require("../assertEqual");
 const eqObjects = require("../eqObjects");
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
-
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
-
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-
-assertEqual(eqObjects(cd, dc), true); // => true
-
 const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+
+// Should equal true
+assertEqual(eqObjects(ab, ba), true); 
+
+// Should equal false
+assertEqual(eqObjects(ab, abc), false); 
+
+// Should equal true
+assertEqual(eqObjects(cd, dc), true); 
+
+// Should equal false
+assertEqual(eqObjects(cd, cd2), false);
