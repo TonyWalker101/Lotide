12cd8e9402292cfd9c74274290445d84a1c5691a
Tony <awalker780@gmail.com>
2021-11-11 18:47:55 -0500

    Improved logic to include .splice()

--- takeUntil.js
+++ takeUntil.js
@@ -1,15 +1,13 @@
+// Takes in an array and a callback and returns a slice of the array based off the callback
+
 const takeUntil = function(array, callback) {
   
-  const newArray = [];
-
-  for (let item in array) {
-    if (!callback(array[item])) {
-      newArray.push(array[item]);
-    } else {
-      return newArray;
-    }
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      return array.slice(0, i);
+    } 
   }
-  return newArray;
+  return array;
 };
 
 module.exports = takeUntil;
