8c310415353e4f68a9317e2db96261849fd35afa
Tony <awalker780@gmail.com>
2021-11-11 17:46:17 -0500

    Added comments

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,7 @@
 const eqArrays = require("./eqArrays");
 
+// Takes in 2 arrays and prints Passed if eqArrays returns True and Failed if eqArrays returns false
+
 let assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     return console.log(`Assertion Passed: ✅ ${actual} === ${expected}`);
