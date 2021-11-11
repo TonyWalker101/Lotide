7cae4cfb0b57237bed877b301f48ccbfd28cb9e2
Tony <awalker780@gmail.com>
2021-11-11 17:57:07 -0500

    Added clearer comments

--- test/without.test.js
+++ test/without.test.js
@@ -1,10 +1,16 @@
 const assertArraysEqual = require("../assertArraysEqual");
 const without = require("../without");
 
-without([1, 2, 3], [1]); // => [2, 3]
-without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+// Should equal [2, 3]
+without([1, 2, 3], [1]); 
 
+// Should equal ["1", "2"]
+without(["1", "2", "3"], [1, 2, "3"]); 
+
+
+// Should equal ["hello","world"]
 const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
+without(words, ["lighthouse"]); 
+
+// Makes sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
