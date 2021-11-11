8cc8bb095e33085561b634869ddf7a1249803760
Tony <awalker780@gmail.com>
2021-11-11 18:09:21 -0500

    Added comments

--- test/countOnly.test.js
+++ test/countOnly.test.js
@@ -15,7 +15,14 @@ const firstNames = [
 
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false});
 
+// Should equal 1
 assertEqual(result1["Jason"], 1);
+
+// Should equal undefined
 assertEqual(result1["Karima"], undefined);
+
+// Should equal 2
 assertEqual(result1["Fang"],2);
+
+// Should equal undefined
 assertEqual(result1["Agouhanna"], undefined);
