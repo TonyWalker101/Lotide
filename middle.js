66f7a78233d07ef8750a2cd0bf4bb49cc00a7f56
Tony <awalker780@gmail.com>
2021-11-11 23:29:34 -0500

    Added more comments

--- middle.js
+++ middle.js
@@ -1,10 +1,14 @@
-// Helper function that gets Middle Index of an Array
+// Function takes in an array and returns the middle most element(s) of the array
+
+// Helper function that gets middle index of an Array
 
 const getMiddleIndex = arr => {
 
   return Math.floor(arr.length / 2);
 
 };
+
+// Main Function
 
 const middle = arr => {
 
