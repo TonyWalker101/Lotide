be37beb6080970a852a69b45a2edd829f2f78616
Tony <awalker780@gmail.com>
2021-11-12 18:55:53 -0500

    Fixed comments

--- test/assertObjectsEqual.test.js
+++ test/assertObjectsEqual.test.js
@@ -4,8 +4,8 @@ const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
 
-// Should equal true
+// Should print Assertion Passed
 assertObjectsEqual((cd, dc), { d: [ '2', 3 ], c: '1' }); 
 
-// Should equal false
+// Should print Assertion Passed
 assertObjectsEqual((cd, cd2), { c: '1', d: [ '2', 3, 4 ] }); 
