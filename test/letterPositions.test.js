ab9746c1914e39e4985467bcb69677462f52dfc4
Tony <awalker780@gmail.com>
2021-11-11 22:39:35 -0500

    Changed .e to [e]

--- test/letterPositions.test.js
+++ test/letterPositions.test.js
@@ -4,4 +4,4 @@ const letterPositions = require("../letterPositions");
 console.log(letterPositions("Lighthouse in the house"));
 
 // Should equal 1
-console.log(assertArraysEqual(letterPositions("hello").e, [1]));
+assertArraysEqual(letterPositions("hello")["e"], [1]);
