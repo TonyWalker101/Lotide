8ebf1570752d2331b0891bba6667c841014f1927
Tony <awalker780@gmail.com>
2021-11-12 18:59:42 -0500

    Fixed comments

--- test/eqArrays.test.js
+++ test/eqArrays.test.js
@@ -1,8 +1,14 @@
 const assertEqual = require("../assertEqual");
 const eqArrays = require("../eqArrays");
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+// Should return true
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
+// Should return false
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); 
+
+// Should return true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+
+// Should return false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
