4f2a148f68a2631fbad3ab2650f2d03203076da8
Tony <awalker780@gmail.com>
2021-11-12 18:51:00 -0500

    Re-added return calls

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -7,9 +7,9 @@ const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect; // the util library's inspect function
 
   if (eqObjects(actual, expected)) {
-    console.log(`Assertion Passed: ✅ ${inspect(actual)} === ${inspect(expected)}`);
+    return console.log(`Assertion Passed: ✅ ${inspect(actual)} === ${inspect(expected)}`);
   }
-  console.log(`Assertion Failed: 🛑 ${inspect(actual)} !== ${inspect(expected)}`);
+  return console.log(`Assertion Failed: 🛑 ${inspect(actual)} !== ${inspect(expected)}`);
 };
 
 module.exports = assertObjectsEqual;
