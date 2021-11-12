2a5b6f30ac9906c5b6cba076e48b2dade6b0bb62
Tony <awalker780@gmail.com>
2021-11-11 22:47:13 -0500

    Amended comment locations

--- test/assertArraysEqual.test.js
+++ test/assertArraysEqual.test.js
@@ -1,6 +1,13 @@
 const assertArraysEqual = require("../assertArraysEqual");
 
-assertArraysEqual([0],[0]); // prints Asserion Passed
-assertArraysEqual([1,2,3,"4"],[1,2,3,4]); // prints Assertion Failed
-assertArraysEqual(["apples", "bananas"], ["apples", "bananas"]); // prints Assertion Passed
-assertArraysEqual([], ["apples"]); // prints Assertion Failed
+// prints Asserion Passed
+assertArraysEqual([0],[0]); 
+
+// prints Assertion Failed
+assertArraysEqual([1,2,3,"4"],[1,2,3,4]); 
+
+// prints Assertion Passed
+assertArraysEqual(["apples", "bananas"], ["apples", "bananas"]); 
+
+// prints Assertion Failed
+assertArraysEqual([], ["apples"]); 
