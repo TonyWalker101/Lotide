22fadf7c3b934257369ff9b20ab45d9b636f4569
Tony <awalker780@gmail.com>
2021-11-12 18:57:49 -0500

    Fixed comments

--- test/countLetters.test.js
+++ test/countLetters.test.js
@@ -1,8 +1,9 @@
 const assertEqual = require("../assertEqual");
 const counterLetters = require("../countLetters");
 
+// Prints by-product of function
 console.log(counterLetters("Hello World"));
 console.log(counterLetters("lighthouse in the house"));
 
-// Should equal 1
+// Should print Assertion Passed
 assertEqual(counterLetters("H")["H"], 1);
