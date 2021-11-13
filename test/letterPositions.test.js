b450969a4f93f1feaa77786eaca59ddf070428ef
Tony <awalker780@gmail.com>
2021-11-12 19:00:56 -0500

    Added comments

--- test/letterPositions.test.js
+++ test/letterPositions.test.js
@@ -1,6 +1,7 @@
 const assertArraysEqual = require("../assertArraysEqual");
 const letterPositions = require("../letterPositions");
 
+// Prints by-product of function
 console.log(letterPositions("Lighthouse in the house"));
 
 // Should equal 1
