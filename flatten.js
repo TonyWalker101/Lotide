0ac2e2c24d60cc704e3c17a034546bf4381ce805
Tony <awalker780@gmail.com>
2021-11-22 18:52:35 -0500

    Improve logic to include recursion

--- flatten.js
+++ flatten.js
@@ -1,18 +1,29 @@
 // Takes in an array that contains nested arrays and returns a "flattened" version of the array (contains no nested arrays)
 
-const flatten = list => {
+const flatten = arr => {
+
   let newArray = [];
 
-  for (let i = 0; i < list.length; i++) {
-    
-    if (!Array.isArray(list[i])) { // action if list[i] isn't an Array
-      newArray.push(list[i]);
+  //Helper function to benefit from recursion without duplicating newArray
+  const collector = array => {
+
+    for (let item of array) {
+  
+      if (Array.isArray(item)) {
+
+        collector(item);
+
+      } else {
+        newArray.push(item);
+      }
     }
-    for (let x = 0; x < list[i].length; x++) { // action if list[i] is an Array
-      newArray.push(list[i][x]);
-    }
-  }
+  };
+
+  collector(arr);
+
   return newArray;
 };
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
 
 module.exports = flatten;
